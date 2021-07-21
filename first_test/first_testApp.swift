9ec0be708923047494945bac3798b0d2ed5a2bc8
jsh <jishikawa.af@gmail.com>
2021-07-21 16:16:12 +0900

    Initial Commit

---
+++ first_test/first_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_testApp.swift
+//  first_test
+//
+//  Created by Jun Ishikawa on 2021/07/21.
+//
+
+import SwiftUI
+
+@main
+struct first_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
