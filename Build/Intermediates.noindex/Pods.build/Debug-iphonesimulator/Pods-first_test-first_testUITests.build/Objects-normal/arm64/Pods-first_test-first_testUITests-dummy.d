3144547e9def3a523d816bcb2460026999eb00b7
jsh <jishikawa.af@gmail.com>
2021-07-30 19:45:34 +0900

    update

---
+++ Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-first_test-first_testUITests.build/Objects-normal/arm64/Pods-first_test-first_testUITests-dummy.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/jun/Developments/swift/first_test/Pods/Target\ Support\ Files/Pods-first_test-first_testUITests/Pods-first_test-first_testUITests-dummy.m \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
+  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator14.5.sdk/usr/include/mach-o/dyld.modulemap
