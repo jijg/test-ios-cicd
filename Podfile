d838300395158c27c85fb0d11ffb9fe522cee95b
jsh <jishikawa.af@gmail.com>
2021-07-30 15:58:07 +0900

    update

--- Podfile
+++ Podfile
@@ -3,7 +3,7 @@
 
 target 'first_test' do
   # Comment the next line if you don't want to use dynamic frameworks
-  use_frameworks!
+#  use_frameworks!
 
   # Pods for first_test
 
---
+++ Pods/Target Support Files/Pods-first_test-first_testUITests/Pods-first_test-first_testUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_first_test_first_testUITests : NSObject
+@end
+@implementation PodsDummy_Pods_first_test_first_testUITests
+@end
