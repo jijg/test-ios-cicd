3ba98fc1da4b04ca5a096fec59ac58382b6a9744
jsh <jishikawa.af@gmail.com>
2021-07-30 19:37:46 +0900

    update

--- Build/Intermediates.noindex/Pods.build/Debug-iphoneos/Pods-first_test.build/Objects-normal/arm64/Pods-first_test-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/jun/Developments/swift/first_test/Pods/Target\ Support\ Files/Pods-first_test/Pods-first_test-dummy.m \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/compact_unwind_encoding.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS14.5.sdk/usr/include/mach-o/dyld.modulemap
